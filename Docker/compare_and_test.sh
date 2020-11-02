3490a0795f2ef39e9b73921b98562c12364a7a27
guillaume2 <guillaume.fernandez@objectif-pi.com>
2020-11-02 15:28:48 +0100

    synchro

--- Docker/compare_and_test.sh
+++ Docker/compare_and_test.sh
@@ -1,11 +1,68 @@
 #!/bin/bash
 
-#Docker dir
-ls -d ../result/* | xargs sha1sum | sort -u -k1,1 | awk '{print $2}' > ./uniq.txt
+#Idee: Django web site, PSQL, scan en python, analyser les fichiers avec python, map country + map mental
 
-for i in ./uniq.csv; do
-  docker run -d --name $i Copy zip as source.zip.
-  sha1sum dir monté
-  Wait for user action / enter
-  sha1sum dir and compare
-  credentials are probably locatate inside docker. If no diff => maybe online
+#Docker dir
+ls -d ./result/* | xargs sha1sum | sort -u -k1,1 | awk '{print $2}' > ./uniq.txt
+
+docker network create --internal --subnet 10.1.1.0/24 no-internet
+
+init_func(){
+  #Init
+  docker run --rm -d -t --net no-internet -p 127.0.0.1:8080:80 --name phis-launcher phis-launcher:latest
+  #--net no-internet
+  docker exec phis-launcher rm /var/www/html/index.html
+  docker cp $1 phis-launcher:/tmp/source.zip
+  docker exec phis-launcher mkdir /tmp/unzip
+  docker exec phis-launcher unzip /tmp/source.zip -d /tmp/unzip/
+  docker exec phis-launcher unzip /tmp/source.zip -d /var/www/html/
+  docker exec phis-launcher service apache2 start
+}
+
+diff_func(){
+  docker exec phis-launcher diff -r --new-file /tmp/unzip/ /var/www/html/ | grep '>'
+  r=$(docker exec phis-launcher bash -c 'diff -r --new-file /var/www/html/ /tmp/unzip/ > /dev/null; echo $?') #With bash -c and $? do not use "", use ''
+  if [ $r == "1" ]
+  then
+    echo "diff detected"
+    return 1
+  else
+    echo "No diff detected"
+    return 0
+  fi
+}
+
+while read i ; do
+  init_func $i
+
+
+  web_files=$(docker exec phis-launcher bash -c "find /var/www/html/ -name '*.php*' -o -name '*.html*' -o -name '*.htm*' | sed 's|/var/www/html||g'") # => /dir1/dir2/file.php /dir/dir3/file2.html etc.
+  for i in $web_files; do
+    echo "http://localhost:8080$i"
+    #Grep every var names insite this file
+    #Get action dest.php
+    #
+    # docker exec phis-launcher bash -c "curl 'http://localhost:80'$i --data-raw 'email=toto%40gmail.com&password=totopass&user=toto'" #check -d for post and get ?
+    # r=$(diff_func)
+    # echo $r
+  done
+  # docker stop phis-launcher
+  exit 1
+done < ./uniq.txt
+
+
+# docker exec phis-launcher bash -c "echo 'user:azerty1234' > /var/www/html/r.txt"
+# docker exec phis-launcher bash -c "echo 'TEST-TEST' > /var/www/html/Adobe1/Adobe1/login.php"
+#
+
+
+
+# r=$(docker exec phis-launcher bash -c "ls -d /var/www/html/*")
+# if [ $($r | wc -w) == 1 ]
+#   r=$(docker exec phis-launcher bash -c "type $r")
+#   if [ $y == True ]
+# then
+#   echo "go recursiv"
+# else
+#   echo "test file there"
+# fi
